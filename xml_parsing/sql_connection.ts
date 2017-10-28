bf420d4e84d480eb74add4918cefdb525839cb0c
jmalish <jordanmalish@gmail.com>
2017-10-28 17:12:16 -0400

    Add sql connection stuff, clean up weapon parsing

---
+++ xml_parsing/sql_connection.ts
@@ -0,0 +1,52 @@
+import * as mysql from "mysql";
+
+let secrets: any = require("./../secrets.json");
+
+let pool: any = mysql.createPool({
+    connectionLimit: 100,
+    host: secrets.db.host,
+    user: secrets.db.username,
+    password: secrets.db.password,
+    database: secrets.db.database,
+    debug: false
+});
+
+export namespace SQL {
+    export function selectFromDatabase(req: any, res: any, query: string) {
+        pool.getConnection(function (err: any, connection: any) {
+            if (err) {
+                res.json({"code": 100, "status": "Error in connection database"});
+                return;
+            }
+
+            connection.on("error", function (err: any) {
+                res.json({"code": 100, "status": "Error in connection database"});
+            });
+
+            connection.query(query, function (err: any, rows: any) {
+                connection.release();
+                if (!err) {
+                    res.json(rows);
+                }
+            });
+        });
+    }
+
+    export function insertIntoDatabase(query: string) { // this is super dirty, and actually probably super dangerous
+        pool.getConnection(function (err: any, connection: any) {
+            if (err) {
+                console.error(err.stack);
+                return;
+            }
+
+            connection.on("error", function (err: any) {
+                console.error(err.stack);
+            });
+
+            connection.query(query, function (err: any) {
+                connection.release();
+                if (err) console.error(err.stack);
+            });
+        });
+    }
+}
--- xml_parsing/weapons.ts
+++ xml_parsing/weapons.ts
@@ -1,12 +1,10 @@
 import * as fs from "fs";
-import * as http from "http";
-import * as Promise from "bluebird";
+// import * as http from "http";
+// import * as Promise from "bluebird";
 import * as xml from "xml2js";
+// import * as sql from "./sql_connection";
 
 let weaponsFile = "./Weapons.xml";
-
-let Key, Name, Description, SkillKey, Damage, Crit, RangeValue, Encumbrance, HP, Price, Rarity, Type;
-
 
 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 fs.readFile(weaponsFile, "utf8", function (err: any, data: any) {
@@ -15,30 +13,8 @@ fs.readFile(weaponsFile, "utf8", function (err: any, data: any) {
     xml.parseString(data, function (err, result) {
         if (err) return console.log(err);
 
-        Key = result["Weapons"]["Weapon"][0]["Key"];
-        Name = result["Weapons"]["Weapon"][0]["Name"];
-        Description = result["Weapons"]["Weapon"][0]["Description"];
-        SkillKey = result["Weapons"]["Weapon"][0]["SkillKey"];
-        Damage = result["Weapons"]["Weapon"][0]["Damage"];
-        Crit = result["Weapons"]["Weapon"][0]["Crit"];
-        RangeValue = result["Weapons"]["Weapon"][0]["RangeValue"];
-        Encumbrance = result["Weapons"]["Weapon"][0]["Encumbrance"];
-        HP = result["Weapons"]["Weapon"][0]["HP"];
-        Price = result["Weapons"]["Weapon"][0]["Price"];
-        Rarity = result["Weapons"]["Weapon"][0]["Rarity"];
-        Type = result["Weapons"]["Weapon"][0]["Type"];
-
-        console.log("Key: " + Key);
-        console.log("Name: " + Name);
-        console.log("Description: " + Description);
-        console.log("SkillKey: " + SkillKey);
-        console.log("Damage: " + Damage);
-        console.log("Crit: " + Crit);
-        console.log("RangeValue: " + RangeValue);
-        console.log("Encumbrance: " + Encumbrance);
-        console.log("HP: " + HP);
-        console.log("Price: " + Price);
-        console.log("Rarity: " + Rarity);
-        console.log("Type: " + Type);
+        for (let weapon of result["Weapons"]["Weapon"]) {  // for each weapon in list
+            console.log(weapon["Name"][0]);  // the [0] gets the value, instead of including all the [''] junk
+        }
     });
 });
